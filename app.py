81c35a60d9b372773c0bbab66b8b7d9bdb58609c
Mohit <sahumohit2005@gmail.com>
2025-08-31 22:43:44 +0530

    Added image preloading + compression fixes

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def new_game():
     return jsonify(deck=deck, mode=mode)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
